894a5506db679857fc0ca477c4b8fe9f67b37600
Jonathan Dupuy <etu.jdupuy@gmail.com>
2013-02-04 16:13:58 -0500

    ssao done

--- src/shaders/ssgi.glsl
+++ src/shaders/ssgi.glsl
@@ -1,12 +1,41 @@
 #version 420
 
+#ifndef SAMPLE_CNT
+#	error SAMPLE_CNT undefined
+#endif
+
+
 // --------------------------------------------------
 // Uniforms
 // --------------------------------------------------
+uniform sampler2D sNoise;  // random vectors
 uniform sampler2DRect sNd; // normal + depth
 uniform sampler2DRect sKa; // albedo
+
 uniform vec2 uScreenSize;  // screen dimensions
-uniform vec3 uLightDir;    // ligh pos in view space
+uniform vec2 uClipZ;       // clipping planes
+uniform vec2 uTanFovs;     // tangent of field of views
+uniform vec3 uLightPos;    // ligh pos in view space
+uniform int uSampleCnt;    // number of samples
+uniform float uRadius;     // radius size
+
+layout(std140)
+uniform Samples {
+	vec4 uSamples[SAMPLE_CNT]; // sampling directions
+};
+
+// --------------------------------------------------
+// Functions
+// --------------------------------------------------
+// get the view position of a sample from its depth 
+// and eye information
+vec3 ndc_to_view(vec2 ndc,
+                 float depth,
+                 vec2 clipPlanes,
+                 vec2 tanFov);
+vec2 view_to_ndc(vec3 view,
+                 vec2 clipPlanes,
+                 vec2 tanFov);
 
 
 // --------------------------------------------------
@@ -29,13 +58,64 @@ layout(location=0) in vec2 iTexCoord;
 layout(location=0) out vec4 oColour;
 void main() {
 	vec2 st = iTexCoord*uScreenSize;
-	vec4 t1 = texture(sNd,st);
-	vec3 t2 = texture(sKa,st).rgb;
-	oColour.rgb = t2;
-//	oColour.rgb = t1.aaaa;
-	vec3 n = t1.rgb*2.0-1.0;
-	float l = dot(uLightDir, n);
-	oColour*= max(0.0,l);
+	vec4 t1 = texture(sNd,st);      // read normal + depth
+	vec3 t2 = texture(sKa,st).rgb; // colour
+	vec3 n = t1.rgb*2.0-1.0; // rebuild normal
+	vec3 p = ndc_to_view(iTexCoord*2.0-1.0, t1.a, uClipZ, uTanFovs); // get view pos
+	vec3 l = uLightPos - p; // light vec
+	vec3 nl = normalize(l);
+	float att = 1.0+1e-3*length(l);
+	oColour.rgb = 2.0*t2*max(0.0,dot(nl,n))/(att*att);
+
+#if defined GI_SSAO
+	float occ = 0.0;
+	float occCnt = 0.0;
+	vec3 rvec = texture(sNoise, gl_FragCoord.xy/64.0).rgb*2.0-1.0;
+	for(int i=0; i<uSampleCnt; ++i) {
+		vec3 dir = reflect(uSamples[i].xyz,rvec); // a la Crysis
+		dir -= 2.0*dir*step(dot(n,dir),0.0);      // a la Starcraft
+		vec3 sp = p + (dir * uRadius) * (t1.a * 1e2); // scale radius with depth
+		vec2 spNdc = view_to_ndc(sp, uClipZ, uTanFovs); // get sample ndc coords
+		bvec4 outOfScreen = bvec4(false); // check if sample projects to screen
+		outOfScreen.xy = lessThan(spNdc, vec2(-1));
+		outOfScreen.zw = greaterThan(spNdc, vec2(1));
+		if(any(outOfScreen)) continue;
+		vec4 spNd = texture(sNd,(spNdc*0.5 + 0.5)*uScreenSize); // get nd data
+		if(spNd.a >= 1.0) continue;
+		vec3 occEye = -sp/sp.z*(spNd.a*uClipZ.x+uClipZ.y); // compute correct pos
+//		vec3 occEye = ndc_to_view(spNdc,spNd.a,uClipZ,uTanFovs); // compute correct pos
+		vec3 occVec = occEye - p; // vector
+		float att2 = 1.0+1e-3*length(occVec); // quadratic attenuation
+		occ += max(0.0,dot(normalize(occVec),n)-0.42) / (att2*att2);
+		++occCnt;
+	};
+	oColour.rgb= occCnt > 0.0 ? vec3(1.0-occ/occCnt) : vec3(1);
+#elif defined GI_SSDO
+	oColour = vec4(1);
+#endif // GI_SSAO
 }
 #endif // _FRAGMENT_
+
+
+// --------------------------------------------------
+// Functions impl.
+// --------------------------------------------------
+vec3 ndc_to_view(vec2 ndc,
+                 float depth,
+                 vec2 clipPlanes,
+                 vec2 tanFov) {
+	// go from [0,1] to [zNear, zFar]
+	float z = depth * clipPlanes.x + clipPlanes.y;
+	// view space position
+	return vec3(ndc * tanFov, -1) * z;
+}
+
+vec2 view_to_ndc(vec3 view,
+                 vec2 clipPlanes,
+                 vec2 tanFov) {
+	return -view.xy / (tanFov*view.z);
+}
+
+
+
 
